97e67617e78b834a46338b7b4ea44cc5f5d1af0a
mmastrac <mmastrac@4e347d80-ee74-11de-8de6-035db03795fd>
2009-12-21 22:57:21 +0000

    Adding template xpi + xpidl dirs

---
+++ xpi/defaults/preferences/dotspots.js
@@ -0,0 +1,12 @@
+// Sets the API service (DO NOT CHANGE)
+pref("extensions.dotspots.domain", '@SITE@');
+
+// Enable debug messages
+pref("extensions.dotspots.debug_messages", false);
+
+// Informational prefs
+pref("extensions.dotspots.build.date", '@DATE@');
+pref("extensions.dotspots.build.suffix", '@SUFFIX@');
+
+// User Agent
+pref("general.useragent.extra.dotspots", 'DotSpots/@DATE@@UACHARCODE@');
