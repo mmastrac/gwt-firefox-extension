428b64fa39478829ab830f42629974c5c693c3bf
mmastrac <mmastrac@4e347d80-ee74-11de-8de6-035db03795fd>
2009-12-22 23:22:19 +0000

    Moving XPI template into war

--- xpi/chrome/content/dotspots/gecko-1.8/script.js
+++
@@ -1,8 +0,0 @@
-const nsIWindowWatcher = Components.interfaces.nsIWindowWatcher;
-
-const CID_WINDOW_WATCHER = "@mozilla.org/embedcomp/window-watcher;1";
-
-var base = "http://localhost:8080/com.dotspots.ExtensionModule/";
-
-var ww = Components.classes[CID_WINDOW_WATCHER].getService(nsIWindowWatcher);
-var win = ww.openWindow(null, "chrome://dotspots/content/extension-hosted.html?gwt.module=" + __gwt_module + "&gwt.base=" + base + "&gwt.codesvr=localhost:9997&gwt.hosted=localhost:9997", "dotSpotsOophm", "chrome", null);
