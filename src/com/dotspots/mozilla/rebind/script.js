a67f5372b35a0aa6e4e6247a5081bf474719bf75
mmastrac <mmastrac@4e347d80-ee74-11de-8de6-035db03795fd>
2009-12-23 23:32:28 +0000

    Remove extra debugging code

--- src/com/dotspots/mozilla/rebind/script.js
+++ src/com/dotspots/mozilla/rebind/script.js
@@ -7,31 +7,9 @@ CID_WINDOW_WATCHER = "@mozilla.org/embedcomp/window-watcher;1";
 var base = "http://localhost:8080/@moduleName/";
 var codeServer = "localhost:9997"
 
-	
-function reportError(message, e) {
-	var errorMessage = e ? e + ((e.stack) ? "\n" + e.stack : "") : "";
-
-	Components.utils.reportError(message + "\n" + errorMessage);
-
-	try {
-		var alertsService = Components.classes[CID_ALERTS_SERVICE]
-				.getService(nsIAlertsService);
-		alertsService.showAlertNotification("", message, errorMessage, false,
-				"", null);
-	} catch (e) {
-	}
-}
-
-reportError('test3');
-
-try {
-	// Open up the extension hosted mode
-	var ww = Components.classes[CID_WINDOW_WATCHER]
-			.getService(nsIWindowWatcher);
-	var win = ww.openWindow(null,
-			"chrome://@moduleName/content/extension-hosted.html"
-					+ "?gwt.module=@moduleName" + "&gwt.base=" + base
-					+ "&gwt.codesvr=" + codeServer, "oophm", "chrome", null);
-} catch (e) {
-	reportError("Error opening window", e);
-}
+// Open up the extension hosted mode
+var ww = Components.classes[CID_WINDOW_WATCHER].getService(nsIWindowWatcher);
+var win = ww.openWindow(null,
+		"chrome://@moduleName/content/extension-hosted.html"
+				+ "?gwt.module=@moduleName" + "&gwt.base=" + base
+				+ "&gwt.codesvr=" + codeServer, "oophm", "chrome", null);
